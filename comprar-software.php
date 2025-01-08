fb9aba1d76fe9bf20dfbcf37ed46c3544eb52fe5
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-08 09:16:24 -0300

    Create comprar-software.php

---
+++ comprar-software.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiSoftwares</title>
+</head>
+<body style="text-align: center;">
+    <h1>Aprovado</h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Approved_img.png" alt="">
+    <p>Software comprado com sucesso</p>
+</body>
+</html>
