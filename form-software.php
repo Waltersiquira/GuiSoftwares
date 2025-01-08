ab06d4d9b25ab7c642319f02e3b62146f6443341
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-08 09:19:43 -0300

    Create form-software.php

---
+++ form-software.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiSoftwares</title>
+</head>
+<body>
+    <form action="enviar-form-software.php" method="get">
+        digite o nome do software <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição do software <br>
+        <input type="text" name="descricao"> <br>
+        digite o preço do software <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem do software <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
